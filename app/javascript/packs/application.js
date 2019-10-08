904356cb571da0b3e42c2e87966fc48dfb895814
Merdzhanov <kmerdjanov@gmail.com>
2019-10-09 00:27:49 +0300

    Add favicon, navigation and Semantic UI setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 require("@rails/ujs").start()
+require("jquery")
 require("turbolinks").start()
+require ("semantic-ui-sass/semantic-ui")
 require("@rails/activestorage").start()
 require("channels")
+
+import "../src/application"
+
+$(document). on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
